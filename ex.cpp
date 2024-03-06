01fe9960c408e68174a103d3669a7ee70d92f177
srinu5713 <96185709+srinu5713@users.noreply.github.com>
2024-03-06 11:47:29 +0530

    Added ex.cpp

---
+++ ex.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Print a message to the terminal
+    std::cout << "Hello, this is a test program!" << std::endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
